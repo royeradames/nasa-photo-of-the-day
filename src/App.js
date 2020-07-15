62e7acea3c9221a47677cfb63c644f50a2b0cced
royeradames <royeraadames@gmail.com>
2020-07-15 17:57:09 -0400

    update function name

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const nasaEveryImgURL = 'https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/iss063e039001.jpg'
   const imgsCollection = ['https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia23984-16.jpg', 'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/50045549941_b8aa2c8417_o.png',  'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/iss063e034203.jpg']
 
-  function pickImgFromImgsCollection (){
+  function pickImgFromCollection (){
     return(
       <div className = 'imgs-collection' >
         {imgsCollection.map((imgUrl) => {
@@ -21,8 +21,8 @@ function App() {
   return (
     <div className="App">
       <EveryDayImg url={nasaEveryImgURL}/>
-      <OtherImgs pickImgFromImgsCollection = {pickImgFromImgsCollection} />
-      
+      <OtherImgs pickImgFromImgsCollection = {pickImgFromCollection} />
+
     </div>
   );
 }
--- src/components/OtherImgs.js
+++ src/components/OtherImgs.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function OtherImgs({pickImgFromImgsCollection}){
+export default function OtherImgs({pickImgFromCollection}){
 
     return(
-        pickImgFromImgsCollection()
+        pickImgFromCollection()
     )
 }
