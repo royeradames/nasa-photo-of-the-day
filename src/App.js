6d727c255586c902682e43b1442b785e5cbf9db6
royeradames <royeraadames@gmail.com>
2020-07-15 22:43:41 -0400

    added mars backup photos file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from "react";
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
+import "./App.css";
 //components
 import EveryDayImg from './components/EveryDayImg'
 import OtherImgs from './components/OtherImgs'
@@ -12,32 +12,35 @@ function App() {
   // const imgsCollection = ['https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia23984-16.jpg', 'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/50045549941_b8aa2c8417_o.png',  'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/iss063e034203.jpg']
 
   // const [nasaEveryImgURL,setNasaEveryImgURL] = useState()
-  const [imgsCollection,setImgsCollection] = useState()
+  const [imgsCollection, setImgsCollection] = useState()
   const marsImgsLink = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=A1HU6rr2pWbHkkOh4HcfsgcVJpRB1Rwqkd3jd9vZ'
-  
-  useEffect( () =>{
+
+  useEffect(() => {
     axios.get(marsImgsLink)
-    .them(resp => {
-      setImgsCollection(resp)
-    })
-    .catch( (err) => {
-      console.log(err)
-    })
+      .then(resp => {
+        console.log(resp.data.photos)
+        // setImgsCollection(resp.data.photos)
+      })
+      .catch((err) => {
+        debugger
+      })
   })
-  function pickImgFromCollection (){
-    return(
-          //react knows how to unpack an array automatically 
-          imgsCollection.map((imgUrl) => {
-            debugger
-            return <img src={imgUrl} alt='Previos Nasa daily' key={imgUrl}></img>
-          })
-    )
-  }
+
+ 
+
+  // function pickImgFromCollection() {
+  //   return (
+  //     //react knows how to unpack an array automatically 
+  //     imgsCollection.map((photo) => {
+  //       return <img src={photo.img_src} alt='Previos Nasa daily' key={photo.id}></img>
+  //     })
+  //   )
+  // }
 
   return (
     <div className="App">
-      <EveryDayImg url={nasaEveryImgURL}/>
-      <OtherImgs pickImgFromCollection = {pickImgFromCollection} />
+      <EveryDayImg url={nasaEveryImgURL} />
+      {/* <OtherImgs pickImgFromCollection = {pickImgFromCollection} /> */}
       <Footer />
     </div>
   )
